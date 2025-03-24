dbfba93eba382dcaed20a59ae088e9e5689b4a39
Vitor Guilherme R. <162996534+Kyra4code@users.noreply.github.com>
2025-03-24 20:08:38 -0300

    Atividade terminada

--- App.js
+++ App.js
@@ -15,6 +15,16 @@ export default function App() {
     setModo(modo === 'back' ? 'front' : 'back');
   }
 
+  async function compartilharFoto(){
+    if(!foto){
+      alert("Tire uma foto para mostra-lá")
+    }
+    if(!(await sharing.isAvailableAsync())){
+      alert("Não foi possivel compartilhar, tente novamente")
+    }
+    await sharing.shareAsync(foto);
+  }
+
   if (!permissao) {
     return <View/>
   }
@@ -23,35 +33,48 @@ export default function App() {
   if(!permissao.granted) {
     return (
       <View style={styles.container}>
-        <Text>Precisamos de sua permissão para tirar foto.</Text>
+        <Text style={styles.message}>Precisamos de sua permissão para tirar foto.</Text>
         <Button onPress={setPermissao} title="Permitir" />
       </View>
     );
   }
 
   return(
-    <View>
-      <CameraView style={{height: 300, width: 300}} ref={cameraRef} type={modo}>
-        <View>
+    <View style={styles.container}>
 
-          <TouchableOpacity onPress={trocarCamera}>
-            <Entypo name="cycle" size={24} color="black" />
-            <Text>Trocar</Text>
+      <CameraView style={styles.camera} ref={cameraRef} facing={modo}>
+
+        <View style={styles.buttonRow}>
+
+          <TouchableOpacity onPress={trocarCamera} style={styles.button}>
+            <Entypo name="cycle" size={24} color='white' />
+            <Text style={styles.text}>Trocar</Text>
           </TouchableOpacity>
 
-          <TouchableOpacity onPress={async () => {
+          <TouchableOpacity style={styles.button} onPress={async () => {
             if(cameraRef.current) {
               const foto = await cameraRef.current.takePictureAsync();
               setFoto(foto.uri);
             }}}>
 
-              <Entypo name="camera" size={24} color="black" />
-              <Text>Tirar foto</Text>
+              <Entypo name="camera" size={24} color="white" />
+              <Text style={styles.text}>Tirar foto</Text>
 
             </TouchableOpacity>
         </View>
+
       </CameraView>
-      {foto && <Image source={{uri: foto}} style={{height: 300, width: 300}} />}
+
+      {foto && (
+        <View style={styles.previewContainer}>
+          <Image source={{uri: foto}} style={styles.fotoView}/>
+
+          <TouchableOpacity onPress={compartilharFoto} style={styles.shareButton}>
+            <Entypo name='compartilhar' color='white' size={24}/>
+            <Text style={styles.shareText}>Compartilhar</Text> 
+          </TouchableOpacity>
+        </View>
+      )}
     </View>
   );
 
@@ -61,9 +84,57 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    justifyContent: 'center'
+  },
+  message:{
+    textAlign: 'center',
+    paddingBottom: 10
+  },
+  camera:{
+    flex: 1
+  },
+  buttonRow:{
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    backgroundColor: 'transparent',
+    marginTop: 5,
+    position: 'absolute',
+    bottom: 50,
+    width: '100%'
+  },
+  button:{
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10
+  },
+  text:{
+    fontSize: 10,
+    fontWeight: 'bold',
+    color: 'white',
+    marginLeft: 5
+  },
+  shareButton:{
+    flexDirection:'row',
+    alignItems: 'center',
+    padding: 10,
+    backgroundColor: '#ff6347',
+    marginLeft: 10,
+    borderRadius: 5
+  },
+  shareText:{
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white',
+    marginLeft: 5
+  },
+  previewContainer:{
+    flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
-    marginTop: StatusBar.currentHeight,
+    marginTop: 10
   },
+  fotoView:{
+    width:200,
+    height: 200
+  }
 });
